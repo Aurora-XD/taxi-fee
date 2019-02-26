0a2f8498fdbf49f04eb9a2385a1e56f3ee06c117
杨晨 <1053364992@qq.com>
2019-02-26 20:32:46 +0800

    本地完成测试

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+/**
+ * 我们考虑出租车计价问题，出租车的运价是每公里0.8元，八公里起会加收50%的每公里运价，起步价是两公里以内6块，停车等待时加收每分钟0.25元，最后计价的时候司机会四舍五入只收整块钱
+ * @param kilometer
+ * @param minute
+ * @returns {number}
+ */
+module.exports = function main(kilometer, minute) {
+    let totalPrice = 6.0;
+    const timesOverBaseKilometer = 0.5;
+    const baseKilometer = 2;
+    const separateKilometer = 8;
+    const pricePerKilometer = 0.8;
+    const pricePerWaitMinute = 0.25;
+
+    if (kilometer > 2 && kilometer <= 8) {
+        totalPrice += pricePerKilometer * (kilometer - baseKilometer)
+    }
+    if (kilometer > 8) {
+        totalPrice += pricePerKilometer * (separateKilometer - baseKilometer) + pricePerKilometer * (1 + timesOverBaseKilometer) * (kilometer - separateKilometer);
+    }
+    totalPrice += pricePerWaitMinute * minute;
+    return Math.round(totalPrice);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,41 @@
 const main = require('../main/main');
+const assert = require('assert');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('return Math.round(6) = 6', function () {
+        let kilometer = 2;
+        let minute = 0;
+        let ret = 6;
+        assert.equal(ret, main(kilometer, minute));
+    }),
+        it('return Math.round(6+0.8*(5-2)=8.4) = 8', function () {
+            let kilometer = 5;
+            let minute = 0;
+            let ret = 8;
+            assert.equal(ret, main(kilometer, minute));
+        }),
+        it('return Math.round(6+(8-2)*0.8+1*(1+0.5)*0.8=12) = 12', function () {
+            let kilometer = 9;
+            let minute = 0;
+            let ret = 12;
+            assert.equal(ret, main(kilometer, minute));
+        }),
+        it('return Math.round(6+0.25*12=9) = 9', function () {
+            let kilometer = 2;
+            let minute = 12;
+            let ret = 9;
+            assert.equal(ret, main(kilometer, minute));
+        }),
+        it('return Math.round(6+0.8*(5-2)+0.25*12=11.4) = 11.4', function () {
+            let kilometer = 5;
+            let minute = 12;
+            let ret = 11;
+            assert.equal(ret, main(kilometer, minute));
+        }),
+        it('return Math.round(6+0.8*(8-2)+0.8*(1+0.5)*(9-8)+0.25*12=15) = 15', function () {
+            let kilometer = 9;
+            let minute = 12;
+            let ret = 15;
+            assert.equal(ret, main(kilometer, minute));
+        })
 });
